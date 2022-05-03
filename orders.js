08f51f92b31621e9deefaa1227555f138b32d230
amirandersonjones <amirandersonjones@gmail.com>
2022-05-03 07:49:06 -0500

    finished

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'King Charles English Spaniel',
+        productNumber: 12345,
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: 'Hound Dog Mix',
+        productNumber: 67890,
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Labrador Retriever',
+        productNumber: 87438,
+        paymentStatus: 'Paid',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Bulldog',
+        productNumber:83678,
+        paymentStatus: 'Refunded',
+        Shipping: 'Declined'
+    },
+    {
+        productName: 'Poodle',
+        productNumber: 836842,
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: 'Golden Retriever',
+        productNumber: 73208,
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+]
